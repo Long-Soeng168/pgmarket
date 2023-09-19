446f974368081ad2fc754f480bf7713b7865e9e9
Long Soeng <longsoeng017@gmail.com>
2023-09-19 23:53:08 +0700

    modified:   app/config/categories.js

--- app/config/categories.js
+++ app/config/categories.js
@@ -19,8 +19,4 @@ export default [
         title: "Category 5",
         img: require("../assets/icons/maintenance.png"),
     },
-    {
-        title: "Category 4",
-        img: require("../assets/icons/maintenance.png"),
-    },
 ];
